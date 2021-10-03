155be497abe4280e83320cc7f88ee84af6634961
parewa <ishanshrestha@gmail.com>
2021-10-03 13:30:29 +0545

    served master

--- slave.bat
+++ slave.bat
@@ -1,2 +1,1 @@
-cd /d %~dp0
 call cmd /k "title Environment && git init && git add -A && git commit -a -m "served master" && git remote add origin https://github.com/Ishan5hrestha/Ishan5hrestha.git || git remote set-url origin https://github.com/Ishan5hrestha/Ishan5hrestha.git && git push origin main" 
